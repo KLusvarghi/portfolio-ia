0a7fb5d8c20428505c6fdfcf03507a9bcd1c527b
KLusvarghi <kauaolusvarghi@gmail.com>
2025-03-19 19:43:36 -0300

    feat(db): criação do banco e tabela com prisma e add tecnologias

---
+++ backend/prisma/migrations/20250319215519_technologies_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "technologies" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "imagem" TEXT NOT NULL,
+    "destaque" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "technologies_pkey" PRIMARY KEY ("id")
+);
